fd15cb146e4a1af40cdfacf1da87e98817a72bbd
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-07-28 12:21:24 -0400

    add clearItemFromCart to CartItem component

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
 import "./CartItem.scss";
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({
+  item: { imageUrl, price, name, quantity },
+  item,
+  clearItem,
+}) => {
   return (
     <div className="CartItem">
       <img src={imageUrl} alt="item" />
@@ -10,9 +16,16 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
         <span className="price">
           {quantity} x ${price}
         </span>
+        <div style={{ cursor: "pointer" }} onClick={() => clearItem(item)}>
+          <i className="fas fa-trash"></i>
+        </div>
       </div>
     </div>
   );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CartItem);
