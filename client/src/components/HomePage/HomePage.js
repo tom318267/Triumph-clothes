f9e10c176aa8341358ad5778ed51083eb01b4361
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-08-04 16:24:21 -0400

    remove react-toastify

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Directory from "../Directory/Directory";
 import "./HomePage.scss";
-import { StyledToastContainer } from "./HomepageToast.styles";
-import { selectSuccessLog } from "../../redux/user/user.selectors";
 
-const HomePage = ({ loggedInSuccess }) => {
-  toast.success("Successful login!");
-
+const HomePage = () => {
   return (
     <div className="HomePage">
-      {loggedInSuccess ? <StyledToastContainer /> : null}
       <h1 className="company tracking-in-expand">TRIUMPH CLOTHING</h1>
       <h2 className="lead text-focus-in">For your authentic self.</h2>
       <Directory />
@@ -21,8 +12,4 @@ const HomePage = ({ loggedInSuccess }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  loggedInSuccess: selectSuccessLog,
-});
-
-export default connect(mapStateToProps)(HomePage);
+export default HomePage;
